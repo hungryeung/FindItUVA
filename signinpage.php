d6745e11b1ba6edc89a47c644b89cf7c24f03d1b
Christopher Yeung <yeung.chris10@gmail.com>
2018-03-29 16:56:13 -0400

    Commented out javascript file (not used)

--- signinpage.php
+++ signinpage.php
@@ -95,6 +95,6 @@
             }
     </script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-</body>
+<!--     <script src="js/bootstrap.min.js"></script>
+ --></body>
 </html>
